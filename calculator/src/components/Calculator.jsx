db4ecaa361c5c319e925b1ea5cda9bb5468def1f
Emilia Mailian <emiliamailian@gmail.com>
2025-04-28 00:43:27 +0200

    små fix samt css

--- calculator/src/components/Calculator.jsx
+++ calculator/src/components/Calculator.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./Calculator.css";
 
 function Calculator() {
   const [tal1, setTal1] = useState("");
@@ -17,9 +18,10 @@ function Calculator() {
   };
 
   return (
-    <div>
+    <div className="calculator"> {}
       <h1>Kalkylator</h1>
-      <h2>Skriv in Tal 1 och Tal 2, välj sedan räknesättet:</h2>
+      <h2 className="instruktion">Skriv in Tal 1 och Tal 2, välj sedan ett räknesätt:</h2>
+      
       <input
         type="number"
         placeholder="Tal 1"
@@ -32,13 +34,15 @@ function Calculator() {
         value={tal2}
         onChange={(e) => setTal2(e.target.value)}
       />
+      
       <div>
         <button onClick={addera}>Addera</button>
         <button onClick={subtrahera}>Subtrahera</button>
         <button onClick={multiplicera}>Multiplicera</button>
         <button onClick={dividera}>Dividera</button>
       </div>
-      <h2>Resultat: {resultat !== null ? resultat : "-"}</h2>
+
+      <h2 className="result">Resultat: {resultat !== null ? resultat : "-"}</h2>
     </div>
   );
 }
